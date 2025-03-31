2e18d89778fa90626396cf3d4481607c0eb73648
mattreu <mateusz12345gru@gmail.com>
2025-03-31 08:36:05 +0200

    field type update

--- application/views/register_form.php
+++ application/views/register_form.php
@@ -23,10 +23,10 @@
                 <input type="text" name="email" value="<?php echo set_value('email'); ?>" size="50" />
 
                 <h5>Password</h5>
-                <input type="text" name="password" value="" size="50" />
+                <input type="password" name="password" value="" size="50" />
 
                 <h5>Password Confirm</h5>
-                <input type="text" name="passconf" value="" size="50" />
+                <input type="password" name="passconf" value="" size="50" />
 
                 <div><input type="submit" value="Submit" /></div>
             </form>
